7d860c051d7518e90d7bad51855debe0dfbdb7b0
Katia Gibson <kcygibson@gmail.com>
2021-07-25 13:47:25 -0400

    reworked comic schema

---
+++ models/comic.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+export {
+  Comic
+}
+
+const comicSchema = new Schema({
+    comicId: Number,
+    title: {
+      type: String,
+      required: true
+    },
+    format: {
+      type: String,
+    },
+    pageCount: {
+      type: Number,
+    },
+    image: String
+  },{
+    timestamps: true
+  })
+
+const Comic = mongoose.model('Comic', comicSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
