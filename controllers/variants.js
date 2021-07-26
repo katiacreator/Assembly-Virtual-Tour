6bbb52f2bb39cfe202f35e1bd5ef35ce7415eaed
Katia Gibson <kcygibson@gmail.com>
2021-07-26 17:09:07 -0400

    hashed api keys for variant controller functions

--- controllers/variants.js
+++ controllers/variants.js
@@ -1,6 +1,12 @@
 import { Variant } from '../models/variant.js'
 import { Comic } from '../models/comic.js'
 import axios from 'axios'
+import md5 from 'md5'
+
+let ts = Date.now()
+let hashKey = md5(`${ts}+${process.env.MARVEL_PRIVATE_API_KEY}+${process.env.MARVEL_PUBLIC_API_KEY}`)
+console.log(`this is my timestamp: ${ts}`)
+console.log(`this is my hashkey: ${hashKey}`)
 
 export {
     index,
