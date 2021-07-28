3a04f9efacba23f9a19df57a5bbe4f1ea00596f6
Katia Gibson <kcygibson@gmail.com>
2021-07-27 21:52:26 -0400

    write alias model

---
+++ models/alias.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+export { Alias };
+
+const aliasSchema = new Schema(
+  {
+    name: String,
+    image: String,
+    alias: String,
+    comicsTotal: { type: Number, default: 0 },
+    seriesTotal: { type: Number, default: 0 },
+    storiesTotal: { type: Number, default: 0 },
+    eventsTotal: { type: Number, default: 0 },
+    //profile will be assigned as an agent to track this alias after completion of the tour
+    agent: [{ type: Schema.Types.ObjectId, ref: "Profile" }],
+    //reference comics Schema which is an array of secret files/comics for this alias
+    comics: [{ type: Schema.Types.ObjectId, ref: "Comic" }],
+    //this is the marvel character id! will need for comics database to MATCH!
+    aliasId: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Alias = mongoose.model("Alias", aliasSchema);
